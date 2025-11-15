a1ce7c48ba72ac960f09d5396682d542b1d4c974
MASTER870-CMD <shashankgowdanb166@gmail.com>
2025-11-15 23:03:55 +0530

    Update app.py

--- Practicaal/app.py
+++ Practicaal/app.py
@@ -8,7 +8,14 @@ import platform
 from datetime import datetime
 from flask import Flask, request, jsonify, send_file, send_from_directory, abort
 from werkzeug.utils import secure_filename
+from flask import Flask, send_from_directory
+import os
 
+app = Flask(__name__, static_folder='frontend', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('frontend', 'index.html')
 
 
 
@@ -558,6 +565,7 @@ def serve_frontend(path):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
